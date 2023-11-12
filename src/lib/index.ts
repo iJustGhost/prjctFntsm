12f4215f4119037348683894c1fcb6a400f354d1
iJustGhost <fryingchicken81@gmail.com>
2023-11-13 00:54:36 +0800

    index

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,6 +3,5 @@
 import { compareSync } from 'bcrypt-ts';
 
 export function enterGate() {
-	console.log(import.meta.env.VITE_SECRET)
 	return compareSync(localStorage.getItem('current')!, import.meta.env.VITE_SECRET);
 }
